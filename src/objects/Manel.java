afc191130cbe12124235c80e11694d5fff0dd8d6
Kaze <kaique.vinicius.profissional@gmail.com>
2024-11-19 18:48:02 +0000

    Files update

---
+++ src/objects/Manel.java
@@ -0,0 +1,41 @@
+package objects;
+
+import java.awt.event.KeyEvent;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import pt.iscte.poo.gui.ImageGUI;
+import pt.iscte.poo.gui.ImageTile;
+import pt.iscte.poo.utils.Direction;
+import pt.iscte.poo.utils.Point2D;
+
+public class Manel implements ImageTile {
+
+	private Point2D position;
+	
+	public Manel(Point2D initialPosition){
+		position = initialPosition;
+	}
+	
+	@Override
+	public String getName() {
+		return "JumpMan";
+	}
+
+	@Override
+	public Point2D getPosition() {
+		return position;
+	}
+
+	@Override
+	public int getLayer() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+	public void move() {
+		int k = ImageGUI.getInstance().keyPressed();
+		position = position.plus(Direction.directionFor(k).asVector());
+	}
+	
+}
